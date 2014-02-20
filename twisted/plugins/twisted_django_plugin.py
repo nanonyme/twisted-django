9ca15ac3b29035bda2ca135c771d4d384c7baa19
Seppo Yli-Olli <seppo.yli-olli@iki.fi>
2014-02-21 01:22:41 +0200

    Renders main page and resources

--- twisted/plugins/twisted_django_plugin.py
+++ twisted/plugins/twisted_django_plugin.py
@@ -8,12 +8,24 @@ from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.web.wsgi import WSGIResource
 from twisted.internet import reactor
-from twisted.web.resource import Resource
+from twisted.web.resource import Resource, getChildForRequest
 from django.conf import settings
 from urlparse import urlparse
 
 class Options(usage.Options):
     optParameters = [["port", "p", 8000, "The port number to listen on."]]
+
+class DjangoResource(Resource):
+    def __init__(self, wsgi, static=None):
+        Resource.__init__(self)
+        self.wsgi = wsgi
+        self.static = static
+
+    def getChild(self, path, request):
+        resource = getChildForRequest(self.static, request)
+        if not hasattr(resource, "isdir") or resource.isdir() or not resource.exists():
+            resource = self.wsgi
+        return resource
 
 class TwistedDjangoServiceMaker(object):
     implements(IServiceMaker, IPlugin)
@@ -29,12 +41,13 @@ class TwistedDjangoServiceMaker(object):
         from django.core.wsgi import get_wsgi_application
         application = get_wsgi_application()
         wsgi_resource = WSGIResource(reactor, reactor.getThreadPool(), application)
-        root = Resource()
+
         static_url = getattr(settings, "STATIC_URL", "")
         if static_url and not urlparse(static_url).scheme:
-            root.putChild(static_url.lstrip("/"), File(settings.STATIC_ROOT))
+            root = DjangoResource(wsgi_resource, File(settings.STATIC_ROOT))
+        else:
+            root = DjangoResource(wsgi_resource)
         site = Site(root)
-        root.putChild("", wsgi_resource)
         return internet.TCPServer(int(options["port"]), site)
 
 serviceMaker = TwistedDjangoServiceMaker()
